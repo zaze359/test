39cb630216e2e5d792083516f95e9eea3d1bc679
zaze359 <359635919@qq.com>
2016-04-23 01:59:03 +0800

    2

---
+++ app/src/main/java/com/zaze/commons/date/DebugDate.java
@@ -0,0 +1,12 @@
+package com.zaze.commons.date;
+
+import java.util.Date;
+
+/**
+ * Created by zaze on 16/4/23.
+ */
+public class DebugDate {
+    public static void debugWeek(){
+        Date date = new Date();
+    }
+}
