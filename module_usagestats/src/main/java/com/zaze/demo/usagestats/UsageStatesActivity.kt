ce69b4b99c9b9682e8aa95d0df02cd7ec4e8a817
zaze <359635919@qq.com>
2021-04-07 18:31:09 +0800

    1

---
+++ module_usagestats/src/main/java/com/zaze/demo/usagestats/UsageStatesActivity.kt
@@ -0,0 +1,16 @@
+package com.zaze.demo.usagestats
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Description :
+ * @author : zaze
+ * @version : 2021-04-07 - 16:52
+ */
+class UsageStatesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
