63f8980dc367bf34989e549a5b02e9c7d2979616
zaze <359635919@qq.com>
2021-04-07 18:33:02 +0800

    1

---
+++ module_usagestats/src/main/java/com/zaze/demo/usagestats/AppUsageUtil.java
@@ -0,0 +1,78 @@
+package com.zaze.demo.usagestats;
+
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class AppUsageUtil {
+    private final static String TAG = "peter.log.AppUsageUtil";
+    private final static String PACKAGE_NAME_UNKNOWN = "unknown";
+
+    public static void checkUsageStateAccessPermission(Context context) {
+        if (!checkAppUsagePermission(context)) {
+            requestAppUsagePermission(context);
+        }
+    }
+
+    public static boolean checkAppUsagePermission(Context context) {
+        UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        if (usageStatsManager == null) {
+            return false;
+        }
+        long currentTime = System.currentTimeMillis();
+        // try to get app usage state in last 1 min
+        List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, currentTime - 60 * 1000, currentTime);
+        if (stats.size() == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void requestAppUsagePermission(Context context) {
+        Intent intent = new Intent(android.provider.Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Log.i(TAG, "Start usage access settings activity fail!");
+        }
+    }
+
+    public static String getTopActivityPackageName(@NonNull Context context) {
+        final UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        if (usageStatsManager == null) {
+            return PACKAGE_NAME_UNKNOWN;
+        }
+
+        String topActivityPackageName = PACKAGE_NAME_UNKNOWN;
+        long time = System.currentTimeMillis();
+        // 查询最后十秒钟使用应用统计数据
+        List<UsageStats> usageStatsList = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 10, time);
+        // 以最后使用时间为标准进行排序
+        if (usageStatsList != null) {
+            SortedMap<Long, UsageStats> sortedMap = new TreeMap<Long, UsageStats>();
+            for (UsageStats usageStats : usageStatsList) {
+                sortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+            }
+            if (sortedMap.size() != 0) {
+                topActivityPackageName = sortedMap.get(sortedMap.lastKey()).getPackageName();
+                Log.d(TAG, "Top activity package name = " + topActivityPackageName);
+            }
+        }
+
+        return topActivityPackageName;
+    }
+}
--- module_usagestats/src/main/java/com/zaze/demo/usagestats/UsageStatesActivity.kt
+++ module_usagestats/src/main/java/com/zaze/demo/usagestats/UsageStatesActivity.kt
@@ -14,3 +14,63 @@ class UsageStatesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
     }
 }
+
+//
+//private Context mContext;
+//private UsageStatsManager mUsageStatsManager;
+//private long mCurrentTime;
+//@Override
+//protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    setContentView(R.layout.activity_main);
+//
+//    mContext = this;
+//    AppUsageUtil.checkUsageStateAccessPermission(mContext);
+//    mUsageStatsManager = (UsageStatsManager) getSystemService(USAGE_STATS_SERVICE);
+//}
+//
+//@Override
+//protected void onResume() {
+//    super.onResume();
+//    mCurrentTime = System.currentTimeMillis();
+//    if(mUsageStatsManager != null) {
+//        queryUsageStats();
+//        queryConfigurations();
+//        queryEventStats();
+//        AppUsageUtil.getTopActivityPackageName(mContext);
+//    }
+//
+//}
+//
+//private void queryUsageStats() {
+//    List<UsageStats> usageStatsList = mUsageStatsManager.queryUsageStats(
+//            UsageStatsManager.INTERVAL_DAILY, mCurrentTime - 60 * 1000, mCurrentTime);
+//    for(UsageStats usageStats: usageStatsList) {
+//        Log.d(TAG,"usageStats PackageName = " + usageStats.getPackageName() + " , FirstTimeStamp = "
+//                + usageStats.getFirstTimeStamp() + " , LastTimeStamp = " + usageStats.getLastTimeStamp()
+//                + ", LastTimeUsed = " + usageStats.getLastTimeUsed()
+//                + " , TotalTimeInForeground = " + usageStats.getTotalTimeInForeground());
+//    }
+//}
+//
+//private void queryConfigurations() {
+//    List<ConfigurationStats> configurationStatsList = mUsageStatsManager.queryConfigurations(
+//            UsageStatsManager.INTERVAL_DAILY,mCurrentTime - 60 * 1000, mCurrentTime);
+//    for (ConfigurationStats configurationStats:configurationStatsList) {
+//        Log.d(TAG,"configurationStats Configuration = " + configurationStats.getConfiguration() + " , ActivationCount = " + configurationStats.getActivationCount()
+//                + " , FirstTimeStamp = " + configurationStats.getFirstTimeStamp() + " , LastTimeStamp = " + configurationStats.getLastTimeStamp()
+//                + " , LastTimeActive = " + configurationStats.getLastTimeActive() + " , TotalTimeActive = " + configurationStats.getTotalTimeActive());
+//    }
+//}
+//
+//private void queryEventStats() {
+//    if (android.os.Build.VERSION.SDK_INT >= 28) {
+//        List<EventStats> eventStatsList = mUsageStatsManager.queryEventStats(
+//                UsageStatsManager.INTERVAL_DAILY,mCurrentTime - 60 * 1000,mCurrentTime);
+//        for(EventStats eventStats:eventStatsList) {
+//            Log.d(TAG,"eventStats EventType" + eventStats.getEventType() + " , Count = " + eventStats.getCount()
+//                    + " , FirstTime = " + eventStats.getFirstTimeStamp() + " , LastTime = " + eventStats.getLastTimeStamp()
+//                    + " , LastEventTime = " + eventStats.getLastEventTime() + " , TotalTime = " + eventStats.getTotalTime());
+//        }
+//    }
+//}
